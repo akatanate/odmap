76ef14970c3f80ef6c004cf9a51527f5043d55e9
akatanate <rinmunakata@gmail.com>
2022-01-31 16:07:59 -0800

    Initial Commit

---
+++ odmap/odmapApp.swift
@@ -0,0 +1,17 @@
+//
+//  odmapApp.swift
+//  odmap
+//
+//  Created by Rina Munakata on 1/31/22.
+//
+
+import SwiftUI
+
+@main
+struct odmapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
